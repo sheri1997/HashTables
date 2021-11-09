0f48ea815b115c97814273bd32cc0c9205aa4872
Shreesh Pandey <shvspandey@gmail.com>
2021-11-09 21:03:11 +0530

    Ability to Find the Frequency of Words in A Sentence

---
+++ NodeMap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    class NodeMap<K,V>
+    {
+        public readonly int size;//will hold the size of the hash table.
+        public readonly LinkedList<KeyValue<K, V>>[] items;
+        public NodeMap(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int getArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            foreach(KeyValue<K,V> item in linkedList)
+            {
+                if(item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V Value)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = Value };
+            linkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach(KeyValue<K,V> item in linkedList)
+            {
+                if(item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if(itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        public LinkedList<KeyValue<K,V>> getLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if(linkedList==null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public struct KeyValue<k,v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashTables
 {
@@ -6,7 +7,17 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Hash Tables Program");
+            NodeMap<string, string> hashTable = new NodeMap<string, string>(5);
+            hashTable.Add("0","To");
+            hashTable.Add("1","be");
+            hashTable.Add("2","or");
+            hashTable.Add("3","not");
+            hashTable.Add("4","to");
+            hashTable.Add("5", "be");
+            string hashTable5 = hashTable.Get("5");
+            Console.WriteLine("5 Value = " + hashTable5);
+            string hashTable2 = hashTable.Get("2");
+            Console.WriteLine("2 Value = " + hashTable2);
         }
     }
 }
